da3c01aa758e3d5ea8ad075f4e8058d02209c4d4
loopies2600 <66535294+loopies2600@users.noreply.github.com>
2020-06-16 07:14:38 -0300

    implementar inputHold

--- scenes/player/PlayerController.gd
+++ scenes/player/PlayerController.gd
@@ -6,7 +6,8 @@ onready var bodyAnim = $Graphics/Body
 onready var arms = $Graphics/Body/Arms
 onready var head = $Graphics/Body/Head
 onready var startPos
-onready var debugDirection = $Graphics/Body/direction
+onready var debugDirection = $Graphics/Debug/direction
+onready var debugHold = $Graphics/Debug/debug_hold
 onready var cooldownTimer = $CooldownTimer
 
 export (PackedScene) var bullet
@@ -41,12 +42,22 @@ func _physics_process(delta):
 	# Variable que comprueba que el jugador no se trato de mover
 	# horizontalmente
 	var friction = false
+
+	# no necesito explicar lo que hace, pero si queres escribirlo
+	var inputHold = false
+	
+	if Input.is_action_pressed("input_hold") and is_on_floor():
+		inputHold = true
+	else:
+		inputHold = false
+		
+	debugHold.visible = inputHold
 	
 	# Recibe input para movimiento horizontal
-	if Input.is_action_pressed("move_right"):
+	if Input.is_action_pressed("move_right") and !inputHold:
 		velocity.x = min(velocity.x + acceleration, maxSpeed)
 		FlipBodyGraphics(false)
-	elif Input.is_action_pressed("move_left"):
+	elif Input.is_action_pressed("move_left") and !inputHold:
 		velocity.x = max(velocity.x - acceleration, -maxSpeed)
 		FlipBodyGraphics(true)
 	else:
@@ -58,7 +69,7 @@ func _physics_process(delta):
 			Respawn()
 		
 		# Salta
-		if Input.is_action_pressed("jump"):
+		if Input.is_action_pressed("jump") and !inputHold:
 			velocity.y -= jumpBoost
 		
 		# Quita velocidad x en el suelo
