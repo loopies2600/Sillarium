b9897e057f3c94ecaea41d6b8addb82310ea09a8
loopies2600 <66535294+loopies2600@users.noreply.github.com>
2020-06-20 09:39:20 -0300

    epilepsia

--- scenes/Levels/Test/testScroll.gd
+++ scenes/Levels/Test/testScroll.gd
@@ -1,10 +1,14 @@
 extends ParallaxBackground
 
-var speed = 0.5
+var speed = 60
 onready var bg = $ParallaxLayer
+var rng = RandomNumberGenerator.new()
 
 func _process(delta):
-	bg.motion_offset.y += speed
+	rng.randomize()
+	var randomColor = rng.randf_range(0.5, 0.75)
+	$ParallaxLayer/BG.modulate.g = randomColor
+	bg.motion_offset.y += speed * delta
 	
 	if (bg.motion_offset.y >= 600):
 		bg.motion_offset.y = 0
