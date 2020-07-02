55c0d1dbe020adbf89d06c22cc540963da469572
Ceniz <Ceniz@gmail.com>
2020-07-02 12:28:03 -0700

    caranon wall crawl (work in progress)

--- scenes/enemy/caranon/CaranonController.gd
+++ scenes/enemy/caranon/CaranonController.gd
@@ -1,31 +1,67 @@
-extends "res://scenes/enemy/BasicEnemyController.gd"
+extends KinematicBody2D
 
-onready var animPlayer = $Graphics/AnimationPlayer
+onready var animPlayer = $Area2D/Graphics/AnimationPlayer
 onready var shotStartPos = $BulletStartPosition
 onready var fireTimer = $ShootTimer
 
 export (PackedScene) var projectile
 export (float) var speed = 50
 export (float) var firingTime = 3
+export (float) var gravityStrength = 50
+export (Vector2) var gravityDirection = Vector2.DOWN
 
-var currentSpeed
+var velocity = Vector2()
+var moving = true
 
 func _ready():
 	fireTimer.connect("timeout", self, "OnFireTimerTimeout")
 	animPlayer.play("Slithering")
 	fireTimer.wait_time = firingTime
 	fireTimer.start()
-	currentSpeed = speed
 
 func _physics_process(delta):
-	position.x += speed * delta
+	HandleMovement(delta)
+
+func HandleMovement(delta):
+	var touchingSurface = false
+	
+	if is_on_wall():
+		rotation = PI * 3 / 2
+		velocity = Vector2(gravityStrength, -speed)
+		touchingSurface = true
+	elif is_on_floor():
+		rotation = 0
+		velocity = Vector2(speed, gravityStrength)
+		touchingSurface = true
+	if is_on_ceiling():
+		rotation = PI
+		velocity = Vector2(-speed, -gravityStrength)
+		touchingSurface = true
+	
+	if !touchingSurface:
+		velocity.y += gravityStrength * delta
+	
+	if moving:
+		velocity = move_and_slide(velocity, Vector2.UP)
+
+func AdjustToFloor():
+	rotation = 0
+	velocity = Vector2(speed, gravityStrength)
+
+func AdjustToWall():
+	rotation = PI*3/2
+	velocity = Vector2(gravityStrength, -speed)
+
+func AdjustToCeiling():
+	rotation = PI * 2
+	velocity = Vector2(-speed, -gravityStrength)
 
 func PlaySlitherAnim():
-	currentSpeed = speed
+	moving = true
 	animPlayer.play("Slithering")
 
 func OnFireTimerTimeout():
-	currentSpeed = 0
+	moving = false
 	animPlayer.play("Firing")
 
 func Fire():
