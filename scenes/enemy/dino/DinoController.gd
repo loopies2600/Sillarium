0c4369c880ef1ec21e112599930c5eb7112b8d35
loopies2600 <66535294+loopies2600@users.noreply.github.com>
2020-08-18 21:07:17 -0300

    el chiquito

--- scenes/enemy/dino/DinoController.gd
+++ scenes/enemy/dino/DinoController.gd
@@ -1,5 +1,8 @@
 extends KinematicBody2D
 
+onready var animSprite = $Body/RightFoot
+onready var animator = $AnimationPlayer
+
 var MAX_SPEED = 50
 var JUMP_MAGNITUDE = 600
 const GRAVITY = 25
@@ -8,61 +11,48 @@ const FRICTION = 0.15
 var motion = Vector2()
 var dir = 1
 var friction = false
-onready var animSprite = $Graphics
 enum states {IDLE, JUMP, LAND}
 var state = states.IDLE
 var stateTimer = 50
 
 func _ready():
-	pass
+	animator.play("Idle")
 	
 func _physics_process(delta):
 	motion.y += GRAVITY
 	
 	motion = move_and_slide(motion, Vector2.UP)
 	
-	animation_checking()
+	stateTimer = 1
 	
-	stateTimer -= 1
+	if (animSprite.frame == 1 || animSprite.frame == 12):
+		animSprite.offset.y = -12
+	else:
+		animSprite.offset.y = 0
+	
 	match state:
 		states.IDLE:
-			animSprite.offset.y = lerp(animSprite.offset.y, 0, 0.1)
-			animSprite.scale = lerp(animSprite.scale, Vector2(1, 1), 0.1)
 			motion.x = lerp(motion.x, 0, 0.1)
 			
 			if stateTimer <= 0:
-				animSprite.offset.y = 0
 				motion.y = -JUMP_MAGNITUDE
 				randomize_movement()
 				change_state(states.JUMP)
 		
 		states.JUMP:
-			animSprite.offset.y = 0
-			
 			match dir:
 				1:
-					animSprite.rotation_degrees += 5
 					motion.x = min(motion.x + ACCELERATION, MAX_SPEED)
-					animSprite.flip_h = true
 				-1:
-					animSprite.rotation_degrees -= 5
 					motion.x = max(motion.x - ACCELERATION, -MAX_SPEED)
-					animSprite.flip_h = false
 			
 			if is_on_floor():
-				animSprite.scale = Vector2(1.1, 0.9)
-				animSprite.offset.y = 48
 				change_state(states.LAND)
 		
 		states.LAND:
 			motion = Vector2.ZERO
-			animSprite.offset.y = lerp(animSprite.offset.y, 24, 0.1)
-			animSprite.scale = lerp(animSprite.scale, Vector2(1, 1), 0.1)
-			animSprite.rotation_degrees = lerp(animSprite.rotation_degrees, 0, 0.75)
 			
 			if stateTimer <= 0:
-				animSprite.offset.y = -16
-				animSprite.scale = Vector2(0.8, 1.2)
 				change_state(states.IDLE)
 				
 func choose_random(int_list):
@@ -76,14 +66,3 @@ func randomize_movement():
 func change_state(new_state):
 	stateTimer = choose_random([8, 16, 32, 64])
 	state = new_state
-	
-func animation_checking():
-	if is_on_floor():
-		match state:
-			states.IDLE:
-				animSprite.play("idle")
-			states.LAND:
-				animSprite.play("land")
-	else:
-		if motion.y <= 0:
-			animSprite.play("jump")
