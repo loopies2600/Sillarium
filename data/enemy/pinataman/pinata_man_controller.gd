2bdd0352388c6ee4602d00e0fd6b8b4ee21a55df
loopies2600 <66535294+loopies2600@users.noreply.github.com>
2020-11-21 15:48:06 -0300

    consistencia en los nombres

---
+++ data/enemy/pinataman/pinata_man_controller.gd
@@ -0,0 +1,25 @@
+extends "../basic_enemy_controller.gd"
+
+onready var animPlayer = $AnimationPlayer
+onready var hitbox = $CollisionShape2D
+
+export (PackedScene) var particle
+
+var velocity = Vector2()
+
+func _ready():
+	connect("DestroySelf", self, "OnDestruction")
+	animPlayer.play("Swinging")
+
+func DisableHitbox():
+	hitbox.disabled = true
+
+func OnDestruction():
+	call_deferred("DisableHitbox")
+	animPlayer.play("Exploding")
+
+func EmitParticles():
+	var newParticles = particle.instance()
+	newParticles.emitting = true
+	get_tree().get_root().add_child(newParticles)
+	newParticles.global_position = global_position
---
+++ globals.gd
@@ -0,0 +1,35 @@
+extends Node
+
+# Para variables globales
+
+const UP = Vector2.UP
+const MAX_FLOOR_ANGLE = deg2rad(15)
+const UNIT_SIZE = 96
+onready var trail = preload("res://data/trail.tscn")
+var debug = true
+
+func LoadLevel(levelName):
+	match levelName:
+		"SalaGraciosa":
+			get_tree().change_scene("res://data/levels/test/puto_gordo_homosexual.tscn")
+		"TestLevel":
+			get_tree().change_scene("res://data/levels/test/test_scene.tscn")
+
+	
+func CreateTrail(fds, tex, pos, rot, scl, z = 0):
+	var newTrail = trail.instance()
+	get_tree().get_root().add_child(newTrail)
+	newTrail.fadeSpeed = fds
+	newTrail.texture = tex
+	newTrail.global_position = pos
+	newTrail.global_rotation = rot
+	newTrail.global_scale = scl
+	newTrail.z_index = z
+
+func LoadJSON(file, index):
+	var jsonFile = File.new()
+	jsonFile.open(file, File.READ)
+	var parsed = JSON.parse(jsonFile.get_as_text())
+	jsonFile.close()
+	
+	return parsed.result[str(index)]
