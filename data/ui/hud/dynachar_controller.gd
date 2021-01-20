53b5aebf31409a7243e0525020e6c64e04c97c2e
L2600 <leandrosotelocraftero@hotmail.com>
2021-01-20 01:26:06 -0300

    dynatext2

--- data/ui/hud/dynachar_controller.gd
+++ data/ui/hud/dynachar_controller.gd
@@ -3,12 +3,10 @@ extends AnimatedSprite
 onready var animator = $Animations
 
 var anim
-var character
 var delay
 var toPos
 
 func _ready():
-	frame = character
 	animator.connect("animation_finished", self, "_animEnd")
 	
 func _process(delta):
@@ -16,11 +14,8 @@ func _process(delta):
 		animator.play(anim)
 	else:
 		delay -= delta
-	
-	if frame != 28:
-		frame = randi() % 28
+
 	position = lerp(position, toPos, delta * 4)
 	
 func _animEnd(anim):
 	set_process(false)
-	frame = character
--- data/ui/hud/dynatext_controller.gd
+++ data/ui/hud/dynatext_controller.gd
@@ -14,7 +14,7 @@ func _ready():
 func _renderText():
 	
 	for c in len(stringu):
-		_spawnCharacter(_charToFrame(stringu[c]), charPos, "Test", 0.1 * c)
+		_spawnCharacter(stringu[c], "Wave1", charPos, 0.1 * c)
 			
 		charPos.x += charWidth
 			
@@ -25,74 +25,10 @@ func _renderText():
 func _unhandled_key_input(event):
 	_restart()
 	
-func _charToFrame(c):
-	match c:
-		"A":
-			return 0
-		"B":
-			return 1
-		"C":
-			return 2
-		"D":
-			return 3
-		"E":
-			return 4
-		"F":
-			return 5
-		"G":
-			return 6
-		"H":
-			return 7
-		"I":
-			return 8
-		"J":
-			return 9
-		"K":
-			return 10
-		"L":
-			return 11
-		"M":
-			return 12
-		"N":
-			return 13
-		"O":
-			return 14
-		"P":
-			return 15
-		"Q":
-			return 16
-		"R":
-			return 17
-		"S":
-			return 18
-		"T":
-			return 19
-		"U":
-			return 20
-		"V":
-			return 21
-		"W":
-			return 22
-		"X":
-			return 23
-		"Y":
-			return 24
-		"Z":
-			return 25
-		"!":
-			return 26
-		"?":
-			return 27
-		" ":
-			return 28
-		"_":
-			return 28
-			print("making a new line..")
-			
-func _spawnCharacter(c, pos, anim, delay):
+func _spawnCharacter(c, anim, pos, delay):
 	var newChar = dynachar.instance()
 	newChar.anim = anim
-	newChar.character = c
+	newChar.animation = c
 	newChar.delay = delay
 	newChar.toPos = pos
 	add_child(newChar)
