b624ea7264dc91a461a1a3ffe469d74ca10ca024
Loopies <66535294+loopies2600@users.noreply.github.com>
2021-01-31 03:22:46 -0300

    lol

--- data/projectile/boomerang_controller.gd
+++ data/projectile/boomerang_controller.gd
@@ -32,6 +32,7 @@ func _physics_process(delta):
 	
 	# Cambia la velocidad
 	if !falling:
+		Objects.spawn(21, sprite.global_position)
 		Renderer.spawnTrail(0.1, sprite.texture, sprite.global_position, sprite.global_rotation, sprite.global_scale, z_as_relative)
 		velocity += velocityDecrease
 	
