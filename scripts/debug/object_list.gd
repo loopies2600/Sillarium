02da7bd81b162a0782e24fe8a86bbed9be870171
Loopies <66535294+loopies2600@users.noreply.github.com>
2021-04-13 22:02:00 -0300

    más cosas para el panel debug

--- scripts/debug/object_list.gd
+++ scripts/debug/object_list.gd
@@ -3,6 +3,7 @@ extends Panel
 onready var lists := [$VBoxContainer/TabContainer/BACKGROUNDS/BGList, $VBoxContainer/TabContainer/MUSIC/MUSICList, $VBoxContainer/TabContainer/OBJECTS/OBJList, $VBoxContainer/TabContainer/PICKUPS/PICKUPList, $VBoxContainer/TabContainer/SCENES/SCENEList, $VBoxContainer/TabContainer/WEATHER/WEATHERList]
 
 var targetJSON := [Renderer.BG, Audio.MUSIC, Objects.OBJ, Objects.PICKUP, Globals.SCENE, Renderer.WEATHER]
+var targetIcons := ["res://sprites/debug/object_list/bg.png", "res://sprites/debug/object_list/music.png", "res://sprites/debug/object_list/obj.png", "res://sprites/debug/object_list/pickup.png", "res://sprites/debug/object_list/scene.png", "res://sprites/debug/object_list/weather.png"]
 
 func _ready():
 	Objects.currentWorld = self
@@ -11,7 +12,7 @@ func _ready():
 		lists[i].connect("item_activated", self, "_on" + lists[i].name + "ItemActivated")
 		
 		for j in range(Globals.getJSONSize(targetJSON[i])):
-			lists[i].add_item(str(Globals.getJSONEntryName(targetJSON[i], j)).to_upper())
+			lists[i].add_item(str(Globals.getJSONEntryName(targetJSON[i], j)).to_upper(), load(targetIcons[i]))
 			
 func _onBGListItemActivated(index):
 	Renderer.backgroundSetup(index)
